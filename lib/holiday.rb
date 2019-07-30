16451f3e79391c0aee25f64412680e49b72bbef9
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 23:20:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,9 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash.each do |key, obj|
-      obj << supply
-end
+  holiday_hash[:winter][]
 end
 
 
