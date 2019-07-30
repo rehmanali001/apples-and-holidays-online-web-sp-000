96d9a8fc0ebd3bfad23c2f0a43c9f0da91645414
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 23:49:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-    holiday_hash.each do |key, obj|
-      key.each do |att, val|
+    holiday_hash.map do |key, obj|
+      key.map do |att, val|
         att if val.include?("BBQ")
   end
 end.flatten.compact
