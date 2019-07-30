01254e461833e605fbcf603b62baec44551304aa
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 23:38:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "#{key}: #{obj}"
 end
 end
-end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
