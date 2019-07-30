78436f6221e39c483b3cc1bb6804e03e4f48eb35
Rehman Ali <rehmanali001@gmail.com>
2019-07-30 23:46:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
     holiday_hash.each do |key, obj|
-      puts "#{key.capitalize}"
+      puts "#{key.capitalize}:"
       obj.each do |attribute, value|
         puts"  #{attribute.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{value.join(", ")}"
 end
